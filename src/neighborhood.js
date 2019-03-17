c552db126964f634dfbad29855567d332c93b2bd
Olivier Rey <rey.olivier@gmail.com>
2019-03-17 08:53:02 +0100

    goingon

--- src/neighborhood.js
+++ src/neighborhood.js
@@ -17,11 +17,18 @@ const { DataFactory } = N3;
 const { namedNode, blankNode, literal, defaultGraph, quad } = DataFactory;
 
 // Define a list of common prefixes
-let PREFIXES = {rdf   : 'PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
-		rdfs  : 'PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>',
-		dc    : 'PREFIX dc:     <http://purl.org/dc/elements/1.1/>',
-                vcard : 'PREFIX vcard:  <http://www.w3.org/2001/vcard-rdf/3.0#>'}
+let REQ_PREFIXES = {
+    rdf   : 'PREFIX rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>',
+    rdfs  : 'PREFIX rdfs:   <http://www.w3.org/2000/01/rdf-schema#>',
+    dc    : 'PREFIX dc:     <http://purl.org/dc/elements/1.1/>',
+    vcard : 'PREFIX vcard:  <http://www.w3.org/2001/vcard-rdf/3.0#>'}
 
+
+let PREFIXES = {
+    rdf   : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    rdfs  : 'http://www.w3.org/2000/01/rdf-schema#',
+    dc    : 'http://purl.org/dc/elements/1.1/',
+    vcard : 'http://www.w3.org/2001/vcard-rdf/3.0#'}
 
 function define_prefix(key, value) {
     PREFIXES[key] = value;
@@ -33,9 +40,36 @@ WHERE { ?s ?p "J.K. Rowling" .}
 */
 
 class neighborhood {
-    constructor(node, graph) {
+    constructor(graph, node, prefix="") {
+	this.both = true;
 	//we have to determine if the node is a NamedNode of a Literal
-	//if node instanceof Literal
+	if (node instanceof Literal) {
+	    this.both = false;
+	}
+	else if (!(node instanceof NamedNode) && !(node instance BlankNode)) {
+	    throw new Error("Unknown type of node: " + node.constructor.name)
+	}
+	
+	if (prefix != ""){
+	    temp = PREFIXES[prefix]
+	    if (temp == undefined)
+		throw new Error("Unknown prefix: " + temp)
+	    else {
+		this.prefix = temp;
+		this.node = "<" + this.prefix + node + ">";
+	    }
+	}
+	else {
+	    this.prefix = "";
+	    this.node = node;
+	}
+	this.graph = graph;
+    }
+    
+    get() {
+	if (this.both == true) {
+	    request1 = "SELECT ?p ?o  WHERE { " + node.value + " .}"
+	}
     }
 }
 
