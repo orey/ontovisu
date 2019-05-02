7ec752ae77b967a49de7e87a6a90ac1a51cb2fff
Olivier Rey <rey.olivier@gmail.com>
2019-05-02 20:00:24 +0200

    Reorganizing

--- fuseki-start.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-echo "Use: source this file."
-echo "source fuseki-start.sh"
-FUSEKIPATH=/home/olivier/Software/jena-fuseki1-1.6.0/
-cd $FUSEKIPATH
-./fuseki-server --config=config.ttl
--- setenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Use: source this file."
-echo "source setenv.sh"
-NODEPATH=/home/olivier/Software/node-v10.15.3-linux-x64/bin/
-export PATH=$NODEPATH:$PATH
-
